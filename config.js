417f77a02286ba231ea38db8da94f3d32be7b15c
AmilcarGit <amilkarurquiagaramos1@gmail.com>
2026-05-31 19:42:13 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,58 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['51910227479', 'AmilcarGit', true],
+  ['51910227479'],
+  ['5493875132593'],
+  ['51910227479']
+]
+
+global.mods = []
+global.prems = []
+
+global.namebot = '𝙴𝙻𝚈𝚂𝚂𝙸𝙰 𝙼𝙳 '
+global.packname = '𝙴𝙻𝚈𝚂𝚂𝙸𝙰 𝙼𝙳 🌸'
+global.author = 'AmilcarGit| © 𝟸𝟶𝟸𝟼 '
+global.moneda = '𝙴𝙻𝚈𝚂𝚂𝙸𝙰 𝙼𝙳'
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.16'
+global.vs = '2.2.0'
+global.sessions = 'sessions'
+global.jadi = 'subsaiyan'
+global.yukiJadibts = true
+
+global.namecanal = '❇️'
+global.idcanal = '120363404707199986@newsletter'
+global.idcanal2 = '120363404707199986@newsletter'
+global.canal = 'https://whatsapp.com/channel/0029Vb7ntULLY6d8uOvyDy0C'
+global.canalreg = '120363404707199986@newsletter'
+
+global.ch = {
+  ch1: '120363404707199986@newsletter'
+}
+
+global.multiplier = 69
+global.maxwarn = 2
+
+global.APIs = {
+  adonix: { url: "https://api-adonix.ultraplus.click", key: "Yuki-WaBot" },
+  vreden: { url: "https://api.vreden.web.id", key: null },
+  nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+  siputzx: { url: "https://api.siputzx.my.id", key: null },
+  delirius: { url: "https://api.delirius.store", key: null },
+  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+  stellar: { url: "https://api.stellarwa.xyz", key: "YukiWaBot", key2: '1bcd4698ce6c75217275c9607f01fd99' },
+  apifaa: { url: "https://api-faa.my.id", key: null },
+  xyro: { url: "https://api.xyro.site", key: null },
+  yupra: { url: "https://api.yupra.my.id", key: null }
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("🌸 Se actualizó 'config.js'"))
+  import(`file://${file}?update=${Date.now()}`)
+})
