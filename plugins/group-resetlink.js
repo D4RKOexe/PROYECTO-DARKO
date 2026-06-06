046216bee11f510ccf6ca244120bb9457dd9171c
El vigilante <cololacalempira5@gmail.com>
2026-06-05 18:04:07 -0600

    Actualizar group-resetlink.js

--- plugins/group-resetlink.js
+++ plugins/group-resetlink.js
@@ -1,15 +1,9 @@
-let handler = async (m, { conn }) => {
-  let who = m.sender
-  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net']
+let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '❀ HINATA RESETLINK ❀\n\nSolo para grupos' }, { quoted: m })
+  if (!isAdmin) return conn.sendMessage(m.chat, { text: '❀ HINATA RESETLINK ❀\n\nSolo administradores' }, { quoted: m })
+  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '❀ HINATA RESETLINK ❀\n\nLa bot necesita ser admin' }, { quoted: m })
 
-  if (!owners.includes(who)) {
-    return conn.sendMessage(m.chat, {
-      text: '❀ HINATA RESETLINK ❀\n\nSolo los creadores pueden usar este comando\n\n> Solicitado por @' + who.split('@')[0],
-      mentions: [who]
-    }, { quoted: m })
-  }
-
-  await conn.sendMessage(m.chat, { text: '⏳ Reseteando link del grupo...' }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: '⏳ Reseteando link...' }, { quoted: m })
 
   try {
     let pp
@@ -21,22 +15,19 @@ let handler = async (m, { conn }) => {
 
     await conn.sendMessage(m.chat, {
       image: { url: pp },
-      caption: '❀ HINATA RESETLINK ❀\n\nLink reseteado correctamente\n\n' + link + '\n\n> Solicitado por @' + who.split('@')[0],
-      mentions: [who]
+      caption: '❀ HINATA RESETLINK ❀\n\nLink reseteado correctamente\n\n' + link
     }, { quoted: m })
 
   } catch (e) {
-    await conn.sendMessage(m.chat, {
-      text: '❀ HINATA RESETLINK ❀\n\nLa bot debe ser admin para resetear el link'
-    }, { quoted: m })
+    await conn.sendMessage(m.chat, { text: '❀ HINATA RESETLINK ❀\n\nNo se pudo resetear el link' }, { quoted: m })
   }
 }
 
 handler.help = ['resetlink']
-handler.tags = ['owner']
+handler.tags = ['group']
 handler.command = /^(resetlink|revoke|nuevolink)$/i
 handler.desc = 'Resetea el link del grupo'
-handler.owner = true
+handler.admin = true
 handler.botAdmin = true
 
 export default handler
