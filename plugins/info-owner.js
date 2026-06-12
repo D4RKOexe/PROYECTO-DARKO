0cc4d01e2f5f6cf869ee24c41177238369dfd1de
El vigilante <cololacalempira5@gmail.com>
2026-06-11 22:26:40 -0600

    Actualizar info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,29 +1,42 @@
 let handler = async (m, { conn }) => {
-  let texto = '𑁍ࠬܓ ⁾ ㅤׄㅤׅㅤׄ HINATA OWNER ㅤ֢ㅤׄㅤׅ\n\n'
-  
-  texto += '👑 » *EL VIGILANTE*\n'
-  texto += '   📱 » +591 77474230\n'
-  texto += '   💎 » Desarrollador Principal\n'
-  texto += '   🇭🇳 » Hola soy de Honduras, creador de HINATA BOT. Me apasiona la tecnología y el anime. Hinata es mi waifu favorita y por eso creamos esta bot con temática de ella. Si tienes sugerencias o problemas, no dudes en contactarnos.\n'
-  texto += '   🐙 » https://github.com/ElvigilanteDv\n\n'
-  
-  texto += '👑 » *BRAYANRK*\n'
-  texto += '   📱 » +57 3223090406\n'
-  texto += '   💎 » Desarrollador Principal\n'
-  texto += '   🇨🇴 » Estudiante de Ingeniería de Software, aprendiendo cada día sobre programación, desarrollo y nuevas tecnologías. Creador de HINATA BOT.\n'
-  texto += '   🐙 » https://github.com/BrayanRK\n\n'
-  
-  texto += '🌸 » *GRUPO OFICIAL*\n'
-  texto += '   💬 » Únete a nuestra comunidad\n'
-  texto += '   📲 » https://chat.whatsapp.com/TU_LINK\n\n'
-  
-  texto += '📦 » *REPOSITORIO*\n'
-  texto += '   🐙 » https://github.com/ElvigilanteDv/Hinata-Bot\n\n'
-  
-  texto += '⫏⫏ HINATA BOT ✿\n\n'
-  texto += '> Contáctanos si tienes dudas ♡'
+  const texto = `𑁍ࠬܓ ⁾ ㅤׄㅤׅㅤׄ HINATA BOT ㅤ֢ㅤׄㅤׅ
 
-  await conn.sendMessage(m.chat, { text: texto }, { quoted: m })
+╭━━━〔 👑 *CREADORES* 〕━━⬣
+┃
+┃ 👑 *EL VIGILANTE* 🇭🇳
+┃ ❀ Desarrollador Principal
+┃ ❀ +591 77474230
+┃ ❀ Hola soy de Honduras, creador de
+┃   HINATA BOT. Me apasiona la tecnología
+┃   y el anime. Hinata es mi waifu favorita.
+┃ ❀ github.com/ElvigilanteDv
+┃
+┃ 👑 *BRAYANRK* 🇨🇴
+┃ ❀ Desarrollador Principal
+┃ ❀ +57 3223090406
+┃ ❀ Estudiante de Ingeniería de Software,
+┃   aprendiendo cada día sobre programación
+┃   y nuevas tecnologías.
+┃ ❀ github.com/BrayanRK
+┃
+╰━━━━━━━━━━━━━━━━━━━━━━⬣
+
+╭━━━〔 🌸 *COMUNIDAD* 〕━━⬣
+┃
+┃ 💬 *Grupo Oficial*
+┃ ❀ https://chat.whatsapp.com/EEppolIlNjGDZrmNyDERRr
+┃
+┃ 📦 *Repositorio*
+┃ ❀ github.com/ElvigilanteDv/Hinata-Bot
+┃
+╰━━━━━━━━━━━━━━━━━━━━━━⬣
+
+> Contáctanos si tienes dudas ♡`
+
+  await conn.sendMessage(m.chat, {
+    image: { url: 'https://files.catbox.moe/yp1tti.png' },
+    caption: texto
+  }, { quoted: m })
 }
 
 handler.help = ['owner']
