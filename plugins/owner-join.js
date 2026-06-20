8d8f2781ff886389447325486de0b403bbbb2977
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-06-19 22:37:29 -0500

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -2,17 +2,17 @@ let linkRegex = /chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
   let who = m.sender
-  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net']
+  let owners = ['51913454004@s.whatsapp.net', '51956768066@s.whatsapp.net']
 
   if (!owners.includes(who)) {
     return conn.sendMessage(m.chat, {
-      text: '🔗 「 HINATA JOIN 」 🔗\n\n💫 » Solo los creadores pueden usar este comando bb'
+      text: '🔗 「 DARKO JOIN 」 🔗\n\n💫 » Solo los creadores pueden usar este comando'
     }, { quoted: m })
   }
 
   if (!text) {
     return conn.sendMessage(m.chat, {
-      text: '🔗 「 HINATA JOIN 」 🔗\n\n💫 » Ingresa el enlace del Grupo bb\n\n> #join https://chat.whatsapp.com/XXXXX'
+      text: '🔗 「 DARKO JOIN 」 🔗\n\n💫 » Ingresa el enlace del Grupo \n\n> #join https://chat.whatsapp.com/XXXXX'
     }, { quoted: m })
   }
 
@@ -22,18 +22,18 @@ let handler = async (m, { conn, text }) => {
 
     if (!code) {
       return conn.sendMessage(m.chat, {
-        text: '🔗 「 HINATA JOIN 」 🔗\n\n🐢 » Enlace inválido bb\n\nEjemplo: https://chat.whatsapp.com/XXXXXXXXXXXXXXXXX'
+        text: '🔗 「 DARKO JOIN 」 🔗\n\n🐢 » Enlace inválido \n\nEjemplo: https://chat.whatsapp.com/XXXXXXXXXXXXXXXXX'
       }, { quoted: m })
     }
 
     await conn.groupAcceptInvite(code)
     await conn.sendMessage(m.chat, {
-      text: '🔗 「 HINATA JOIN 」 🔗\n\n🍭 » El jefe ordenó y la bot obedeció — me uní al grupo bb ✅'
+      text: '🔗 「 DARKO JOIN 」 🔗\n\n🍭 » El jefe ordenó y el bot obedeció — me uní al grupo  ✅'
     }, { quoted: m })
 
   } catch (e) {
     await conn.sendMessage(m.chat, {
-      text: `🔗 「 HINATA JOIN 」 🔗\n\n✘ » Error al unirse bb\n\n_${e.message || e}_`
+      text: `🔗 「 DARKO JOIN 」 🔗\n\n✘ » Error al unirse bb\n\n_${e.message || e}_`
     }, { quoted: m })
   }
 }
@@ -41,7 +41,7 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['join']
 handler.tags = ['owner']
 handler.command = /^(join|entrar)$/i
-handler.desc = 'Une la bot a un grupo bb'
+handler.desc = 'Une el bot a un grupo'
 handler.rowner = true
 
 export default handler
