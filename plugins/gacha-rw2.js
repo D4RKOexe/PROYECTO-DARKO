b26f22e114d11ea7756c4861bdd34c3badd634f6
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-08-18 03:31:04 -0500

    Actualizar gacha-rw2.js

--- plugins/gacha-rw2.js
+++ plugins/gacha-rw2.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn }) => {
     let minutos = Math.floor(tiempoRestante / 60)
     let segundos = tiempoRestante % 60
     return conn.sendMessage(m.chat, {
-      text: '𖣔 「 HINATA GACHA 2 」 ˚ʚ♡ɞ˚\n\n💫 » Espera ' + minutos + 'm ' + segundos + 's'
+      text: '🎖2️⃣ 「 DARKO GACHA 2 」 🎖2️⃣\n\n💫 » Espera ' + minutos + 'm ' + segundos + 's'
     }, { quoted: m })
   }
 
@@ -37,7 +37,7 @@ let handler = async (m, { conn }) => {
     user.inventory2.push(char.name)
     cooldownsRw2[who] = now + 300000
 
-    let texto = '𖣔 「 HINATA GACHA 2 」 ˚ʚ♡ɞ˚\n\n'
+    let texto = '🎖2️⃣ 「 HINATA GACHA 2 」 🎖2️⃣\n\n'
     texto += '  💫 Personaje obtenido\n\n'
     texto += '  ✦ ' + char.name + ' ✦\n'
     texto += '  🎬 Anime: ' + (char.anime || 'Desconocido') + '\n'
